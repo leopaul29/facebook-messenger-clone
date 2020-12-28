d8b793de8a8667ed3e02fb2edfd8e42b8bf456e0
Leo-paul <leopaulmartin@hotmail.fr>
2020-12-28 23:15:10 +0100

    feat: add message header and update css

--- src/Message.js
+++ src/Message.js
@@ -1,27 +1,29 @@
 import React, { forwardRef } from "react";
 import Card from "@material-ui/core/Card";
+import Tooltip from "@material-ui/core/Tooltip";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import "./Message.css";
 
 const Message = forwardRef(({ message, username }, ref) => {
-    const isUser = username === message.username;
-
-    return (
-        <div ref={ref} className={`messages ${isUser && "messages__user"}`}>
-            <Card
-                className={
-                    isUser ? "messages__userCard" : "messages__guestCard"
-                }
-            >
-                <CardContent>
-                    <Typography variant="h5" component="h2">
-                        {!isUser && `${message.username || 'Unknown User'}: `} {message.message}
-                    </Typography>
-                </CardContent>
-            </Card>
+  const isUser = username === message.username;
+  
+  return (
+    <Tooltip title="date" placement="top" arrow>
+      <div ref={ref} className={`messages ${isUser && "messages__user"}`}>
+        <div className="username">
+          {!isUser && `${message.username || "Unknown User"}`}
         </div>
-    );
+        <Card className={isUser ? "messages__userCard" : "messages__guestCard"}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              {message.message}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </Tooltip>
+  );
 });
 
 export default Message;
